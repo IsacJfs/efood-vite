c646d8dd71a10d613c690813ac620e518f4b2956
IsacJfs <isac.jfs@gmail.com>
2023-12-01 17:30:46 -0300

    :bug: [FIX] Acrescentando Overlay na mensagem de sucesso

--- src/features/checkout/components/SuccessMessage.tsx
+++ src/features/checkout/components/SuccessMessage.tsx
@@ -33,31 +33,35 @@ const SuccessMensage = ({ onClose, restartToCart }: SuccessProps) => {
   }
 
   return (
-    <S.Aside>
-      <S.SuccessContainer>
-        <h4>Pedido realizado - {orderId ? orderId : 'Carregando...'}</h4>
-        <p>
-          Estamos felizes em informar que seu pedido já está em processo de
-          preparação e, em breve, será entregue no endereço fornecido.
-        </p>
-        <p>
-          Gostaríamos de ressaltar que nossos entregadores não estão autorizados
-          a realizar cobranças extras.
-        </p>
-        <p>
-          Lembre-se da importância de higienizar as mãos após o recebimento do
-          pedido, garantindo assim sua segurança e bem-estar durante a refeição.
-        </p>
-        <p>
-          Esperamos que desfrute de uma deliciosa e agradável experiência
-          gastronômica. Bom apetite!
-        </p>
-      </S.SuccessContainer>
+    <>
+      <S.Overlay />
+      <S.Aside>
+        <S.SuccessContainer>
+          <h4>Pedido realizado - {orderId ? orderId : 'Carregando...'}</h4>
+          <p>
+            Estamos felizes em informar que seu pedido já está em processo de
+            preparação e, em breve, será entregue no endereço fornecido.
+          </p>
+          <p>
+            Gostaríamos de ressaltar que nossos entregadores não estão
+            autorizados a realizar cobranças extras.
+          </p>
+          <p>
+            Lembre-se da importância de higienizar as mãos após o recebimento do
+            pedido, garantindo assim sua segurança e bem-estar durante a
+            refeição.
+          </p>
+          <p>
+            Esperamos que desfrute de uma deliciosa e agradável experiência
+            gastronômica. Bom apetite!
+          </p>
+        </S.SuccessContainer>
 
-      <S.LinkFinish to="/" onClick={CloseCleanAll}>
-        Concluir
-      </S.LinkFinish>
-    </S.Aside>
+        <S.LinkFinish to="/" onClick={CloseCleanAll}>
+          Concluir
+        </S.LinkFinish>
+      </S.Aside>
+    </>
   )
 }
 
